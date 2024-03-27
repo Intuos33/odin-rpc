143e7d41e1510482ac2a584d968480bb47dda82a
Intuos33 <bmiller.development@gmail.com>
2024-03-27 18:59:37 -0400

    Finished the computer choice useing a random number to get the index of the options in a list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.console.log("Test");
+function getComputerChoice(){
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    let choice = Math.floor(Math.random() * 3);
+    return choices[choice];
+}
+
+getComputerChoice();
