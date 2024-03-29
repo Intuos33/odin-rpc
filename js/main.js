55ad313189d290f8a710cb91f50b7abd9bd3295e
Intuos33 <bmiller.development@gmail.com>
2024-03-28 20:37:03 -0400

    Internet went out

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+let playerCount = 0;
+let computerCount = 0;
+
 function getComputerChoice(){
     let choices = ['Rock', 'Paper', 'Scissors'];
     let choice = Math.floor(Math.random() * 3);
@@ -6,24 +9,51 @@ function getComputerChoice(){
 
 function playRound(computerChoice, playerChoice){
     if(computerChoice == 'Rock' && playerChoice == 'scissors' || computerChoice == 'Paper' && playerChoice == 'rock' || computerChoice == 'Scissors' && playerChoice == 'paper'){
+        computerCount += 1;
         return ('You lose! ' + computerChoice + ' beats ' + playerChoice + '!');
+    } else if(computerChoice == playerChoice) {
+        return ('Its a Draw!')
     } else {
+        playerCount += 1;
         return ('You win! ' + playerChoice + ' beats ' + computerChoice + '!');
     }
 }
 
-function getPlayerChoice(){
-    const choice = prompt('Rock, Paper, or Scissors? ');
-    return choice.toLowerCase();
-}
-
 function playGame(){
-    let game = 0;
-    while(game <= 4){
+
+    while(playerCount <= 5 || computerCount <= 5){
+        const rock = document.querySelector('#rock');
+        const paper = document.querySelector('#paper');
+        const scissors = document.querySelector('#scissors');
+        const output = document.querySelector('div');
+
         const computerChoice = getComputerChoice();
-        const playerChoice = getPlayerChoice();
-        console.log(playRound(computerChoice, playerChoice));
-        game += 1;
+
+        rock.addEventListener('click', () => {
+            const game = playRound(computerChoice, 'rock');
+            output.textContent = game;
+
+        });
+        
+        paper.addEventListener('click', () => {
+            const game = playRound(computerChoice, 'paper');
+            output.textContent = game;
+
+        });
+
+        scissors.addEventListener('click', () => {
+            const game = playRound(computerChoice, 'scissors');
+            output.textContent = game;
+
+        });
+
+        if(playerCount == 5){
+            output.textContent = 'You win! You won 5 rounds!';
+        }
+
+        if(computerCount == 5){
+            output.textContent = 'You lose! Computer won 5 rounds!';
+        }
     }
 }
 
